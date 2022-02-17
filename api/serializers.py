75e0de5264534cb1affdf91830c50f377e06865a
Tea <teabojovic2004@gmail.com>
2022-02-17 10:41:46 +0100

    make serializers

---
+++ api/serializers.py
@@ -0,0 +1,53 @@
+from .models import City, User, Genre, Author, Book, Image, Status, Exchange, Sale
+from rest_framework import serializers
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ("name")
+
+class UserSerializer(serializers.ModelSerializer):
+    city = CitySerializer()
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "django_user", "city")
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ("name")
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ("first_name", "last_name")
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("name", "original_owner", "author", "genre", "edition", "preservation_level")
+class ImageSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many = True)
+    class Meta:
+        model = Image
+        fields = ("image", "book")
+
+class StatusSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Status
+        fields = ("name")
+
+class ExchangeSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many = True)
+    class Meta:
+        model = Exchange
+        fields = ("image", "book")
+
+class SaleSerializer(serializers.ModelSerializer):
+    book = BookSerializer(many = True)
+    status = StatusSerializer()
+    buyer = UserSerializer(many = True)
+    class Meta:
+        model = Sale
+        fields = ("book", "status", "date_published", "date_sold", "price")
+
